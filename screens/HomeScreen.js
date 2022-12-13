ea2ff9d1fbd24be2c636332fad0e3bfcccb0eeeb
japheth-dew <solaadewumi2002@gmail.com>
2022-12-13 14:36:19 +0100

    backing up apps

---
+++ screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          placeholder="Where from?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          minLength={2}
+          autoFocus={false}
+          returnKeyType={"default"}
+          fetchDetails={true}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
